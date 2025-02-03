27911bfdcf01a85ad0feaac8f2e1821396183aae
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-03 17:26:14 +0700

    decorate KuShopTitle

--- src/components/KuShopTitle.tsx
+++ src/components/KuShopTitle.tsx
@@ -1,31 +1,19 @@
-import { View , Text , Image , StyleSheet} from "react-native";
+import { View, Text, Image } from "react-native";
 
-function KuShopTitle({title} : {title: string}) {
+function KuShopTitle({ title }: { title: string }) {
     return (
-                <View style={styles.logoContainer}>
-                    <Image source={require('../../assets/KULogo.png')} style={styles.logo} />
-                    <Text style={styles.title}>{title}</Text>
-                </View>
-    )
+        <View className="items-center mb-10 mt-5 ">
+            <Image
+                source={require('../../assets/KULogo.png')}
+                className="w-24 h-24 object-contain mr-3 mb-10 " // Add margin-right for spacing
+            />
+            <Text className="text-black text-5xl text-center"> 
+                  {title}
+            </Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-    logoContainer: {
-        alignItems: 'center', // Center the logo and title horizontally
-        marginBottom: 20, // Add spacing below the logo-title section
-        marginTop : 10
-    },
-    logo: {
-        width: 100, // Adjust width of the logo
-        height: 100, // Adjust height of the logo
-        resizeMode: 'contain', // Maintain the aspect ratio of the logo
-    },
-    title: {
-        color: 'black',
-        fontSize: 40,
-        marginTop: 10, // Add spacing between the logo and title
-        textAlign: 'center',
-    },
-});
+
 
 export default KuShopTitle;
