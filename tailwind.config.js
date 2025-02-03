93eb8de18e8c75d2d7f57284297b7af1f1b338cc
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-03 17:25:19 +0700

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kuColor: "#BFE7E2"
+      }
+    },
   },
   plugins: [],
 }
