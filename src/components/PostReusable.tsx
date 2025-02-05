5da2c025ced8a602ce6121e19970c5953b073ae5
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-05 16:34:45 +0700

    modified: PostReusable Page

--- src/components/PostReusable.tsx
+++ src/components/PostReusable.tsx
@@ -29,51 +29,27 @@ const PostReusable = ({ post, isMyPosts, className}: { post: Post, isMyPosts?: B
 
     return (
         <Pressable onPress={() => navigation.navigate('PostDetail', { post })}>
-            <View className={`bg-white w-full rounded-2xl mb-4 p-4 ${className}`}>
+            <View className={`bg-white w-full rounded-2xl mb-4 p-4 mt-5 ${className}`}>
                 { isMyPosts && 
                     <TouchableOpacity className="absolute top-2 right-2 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center" onPress={removePostByID}>
                         <Text className="text-white text-xs">X</Text>
                     </TouchableOpacity>
                 }
-
-                <Image source={require("../../assets/defaultPostImg.png")} className="w-full h-40 rounded-md mt-2" resizeMode="cover" />
-                <View style={{ marginBottom: 20 }}>
-                    <Text style={{ marginTop: 5 }}>Title: {post.title}</Text>
-                    {/* <Text style={{ marginTop: 5 }}>Content: {post.content}</Text> */}
-                    <View style={{ display: "flex", flexDirection: "row", alignItems: 'center', marginTop: 5 }}>
+                <Image source={require("../../assets/defaultPostImg.png")} className="w-full h-40 rounded-md mt-6" resizeMode="cover" />
+                <View className='mb-5'>
+                    <Text className='mt-5 mb-2'>Title: {post.title}</Text>
+                    <View className="flex flex-row items-center mt-1 mb-2">
                         <AntDesign name="like1" size={24} color="#004c27" />
-                        <Text style={{ marginLeft: 10 }}>{post.likes}</Text>
+                        <Text className='ml-2 mt-2'>{post.likes}</Text>
                     </View>
-                    <View style={{ display: "flex", flexDirection: "row", alignItems: 'center', marginTop: 5 }}>
+                    <View className="flex flex-row items-center mt-1 mb-2">
                         <ProfileImage size={20} />
-                        <Text style={{ marginLeft: 10 }}>{post.userID}</Text>
+                        <Text className='ml-2 mt-2'>{post.userID}</Text>
                     </View>
                 </View>
             </View>
         </Pressable>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        width: 300,
-        marginTop: 20,
-        display: 'flex',
-        alignItems: 'center',
-        borderRadius: 20,
-        position: 'relative'
-    },
-    deleteButton: {
-        backgroundColor: 'red',
-        position: 'absolute',
-        margin: 10,
-        right: 0,
-        borderRadius: 50,
-        width: 30,
-        display: 'flex',
-        alignItems: 'center'
-    }
-})
 
 export default PostReusable;
