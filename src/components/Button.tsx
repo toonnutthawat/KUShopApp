f08387db194cf29f9030bab90b0c66e41a054fba
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-10 17:41:56 +0700

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View, Pressable, ViewStyle, TextStyle } from 'react-native';
+import { theme } from '../constants/theme';
+import { hp } from '../helpers/common';
+
+interface ButtonProps {
+    buttonStyle?: ViewStyle;
+    textStyle?: TextStyle;
+    title?: string;
+    onPress?: () => void;
+    loading?: boolean;
+    hasShadow?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    buttonStyle,
+    textStyle,
+    title = '',
+    onPress = () => {},
+    loading = false,
+    hasShadow = true,
+}) => {
+    const shadowStyle: ViewStyle = {
+        shadowColor: theme.colors.dark,
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.2,
+        shadowRadius: 8,
+        elevation: 4,
+    };
+
+    if (loading) {
+        return (
+            <View style={[styles.button, buttonStyle, { backgroundColor: theme.colors.kuColor }]}>
+                
+            </View>
+        );
+    }
+
+    return (
+        <Pressable onPress={onPress} style={[styles.button, buttonStyle, hasShadow && shadowStyle]}>
+            <Text style={[styles.text, textStyle]}>{title}</Text>
+        </Pressable>
+    );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: theme.colors.primary,
+        height: hp(6.6),
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: theme.radius.xl
+    },
+    text: {
+        fontSize: hp(2.5),
+        color: 'white',
+        fontWeight: theme.fonts.bold as TextStyle['fontWeight'],
+    },
+});
