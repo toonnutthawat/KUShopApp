47ebb464c4e749a1a98082e1f0ec369ee60a66a7
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-11 15:19:31 +0700

    Edit SignUpPage.tsx

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,8 +1,17 @@
-import { View, TextInput, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, TextInput, Text, TouchableOpacity, StyleSheet, TextStyle, Pressable } from "react-native";
+import { StatusBar } from 'expo-status-bar'
 import { signUp } from "aws-amplify/auth";
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 import ConfirmSignUpPage from "./ConfirmSignUpPage";
 import KuShopTitle from "../components/KuShopTitle";
+import { hp, wp } from "../helpers/common";
+import { theme } from "../constants/theme";
+import ScreenWrapper from "../components/ScreenWrapper";
+import BackButton from "../components/BackButton";
+import Input from "../components/Input";
+import Icon from "../../assets/icons";
+import Button from "../components/Button";
 
 function SignUpPage() {
     const [email, setEmail] = useState("");
@@ -11,6 +20,7 @@ function SignUpPage() {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const [confirmSignUp, setConfirmSignUp] = useState(false);
+    const navigation = useNavigation();
 
     const handleSignUp = async () => {
         try {
@@ -32,60 +42,117 @@ function SignUpPage() {
     };
 
     return (
-        <View className="flex-1 bg-kuBGColor items-center justify-center p-4 pt-10">
-            {confirmSignUp ? (
-                <ConfirmSignUpPage username={username}  email={email}/>
-            ) : (
-                <View className="bg-kuColor w-full h-full rounded-2xl p-6">
-                    <KuShopTitle title="SIGN UP" />
-                    <TextInput
-                        autoComplete="email"
-                        keyboardType="email-address"
-                        placeholder="Email"
-                        value={email}
-                        onChangeText={(value) => setEmail(value)}
-                        className="bg-gray-100 rounded-xl h-12 px-4 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300"
-                        placeholderTextColor="#555"
-                    />
-                    <TextInput
-                        placeholder="Username"
-                        value={username}
-                        onChangeText={(value) => setUsername(value)}
-                        className="bg-gray-100 rounded-xl h-12 px-4 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300"
-                        placeholderTextColor="#555"
-                    />
-                    <TextInput
-                        placeholder="Password"
-                        value={password}
-                        secureTextEntry={true}
-                        onChangeText={(value) => setPassword(value)}
-                        className="bg-gray-100 rounded-xl h-12 px-4 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300"
-                        placeholderTextColor="#555"
-                    />
-                    <TextInput
-                        placeholder="Confirm Password"
-                        value={confirmPassword}
-                        secureTextEntry={true}
-                        onChangeText={(value) => setConfirmPassword(value)}
-                        className="bg-gray-100 rounded-xl h-12 px-4 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300"
-                        placeholderTextColor="#555"
-                    />
-                    {errorMessage && <Text className="text-red-500 mb-4">{errorMessage}</Text>}
-                    <TouchableOpacity
-                        onPress={handleSignUp}
-                        className={`bg-green-500 rounded-lg h-12 justify-center items-center ${
-                            confirmPassword !== password || !password
-                                ? 'opacity-50'
-                                : 'opacity-100'
-                        }`}
-                        disabled={confirmPassword !== password || !password}
-                    >
-                        <Text className="text-white font-bold">Sign Up</Text>
-                    </TouchableOpacity>
-                </View>
-            )}
-        </View>
+        <ScreenWrapper bg = {theme.colors.kuColor}>
+            <StatusBar style = "dark"></StatusBar>
+                {confirmSignUp ? (
+                    <ConfirmSignUpPage username={username}  email={email}/>
+                ) : (
+                    
+                    <View style = {styles.container}>
+                        <BackButton/>
+                            <View>
+                                <Text style ={styles.welcomeText}>Let's</Text>
+                                <Text style ={styles.welcomeText}>Get Started</Text>
+                            </View>
+
+                        <View style = {styles.form}>
+                            <Input
+                                icon = {<Icon name = "mail" size = {26} strokeWidth = {1.6} />}
+                                autoComplete="email"
+                                keyboardType="email-address"
+                                placeholder="Email"
+                                value={email}
+                                onChangeText={(value) => setEmail(value)}     
+                                placeholderTextColor="#555"
+                            />
+                            <Input
+                                icon = {<Icon name = "user" size = {26} strokeWidth = {1.6} />}
+                                placeholder="Username"
+                                value={username}
+                                onChangeText={(value) => setUsername(value)}
+                                placeholderTextColor="#555"
+                            />
+                            <Input
+                                icon = {<Icon name = "lock" size = {26} strokeWidth = {1.6} />}
+                                placeholder="Password"
+                                value={password}
+                                secureTextEntry={true}
+                                onChangeText={(value) => setPassword(value)}
+                                placeholderTextColor="#555"
+                            />
+                            <Input
+                                icon = {<Icon name = "lock" size = {26} strokeWidth = {1.6} />}
+                                placeholder="Confirm Password"
+                                value={confirmPassword}
+                                secureTextEntry={true}
+                                onChangeText={(value) => setConfirmPassword(value)}
+                                placeholderTextColor="#555"
+                            />
+                            {errorMessage && <Text className="text-red-500 mb-4">{errorMessage}</Text>}
+                            
+                            {/* button */}
+                            <Button title = {'Sign up'}  onPress = {handleSignUp}/>
+
+                            {/* footer */}
+                            <View style={styles.footer}>
+                                <Text style = {styles.footerText}>
+                                    Already have an account!
+                                </Text>
+                                <Pressable onPress={() => navigation.navigate("Login" as never)}>
+                                    <Text style={[styles.footerText, {color:theme.colors.primaryDark,fontWeight: theme.fonts.semibold as TextStyle['fontWeight']}]}>Login</Text>
+                                </Pressable>
+                            </View>
+
+                        </View>      
+                    </View>
+                ) 
+            }        
+    </ScreenWrapper>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        gap:45,
+        paddingHorizontal:wp(5),
+    },
+    welcomeText:{
+        fontSize: hp(4),
+        fontWeight: theme.fonts.bold as TextStyle['fontWeight'],
+        color: theme.colors.text,
+    },
+    form:{
+        gap:25,
+    },
+    forgotPassword:{
+        textAlign: 'right',
+        fontWeight: theme.fonts.semibold as TextStyle['fontWeight'],
+        color:theme.colors.text,
+    },
+    footer:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems:'center',
+        gap:5,
+    },
+    footerText:{
+        textAlign: 'center',
+        color : theme.colors.text,
+        fontSize: hp(1.6)
+    },
+    button: {
+        backgroundColor: theme.colors.primary,
+        height: hp(6.6),
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: theme.radius.xl
+    },
+    text: {
+        fontSize: hp(2.5),
+        color: 'white',
+        fontWeight: theme.fonts.bold as TextStyle['fontWeight'],
+    },
+})
 
 export default SignUpPage;
