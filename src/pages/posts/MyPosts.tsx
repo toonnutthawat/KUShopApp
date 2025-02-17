f0d124fc0203a46810e57849613d213e9a7a5f14
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-17 17:41:55 +0700

    modified: Myposts decorate

--- src/pages/posts/MyPosts.tsx
+++ src/pages/posts/MyPosts.tsx
@@ -1,8 +1,9 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput,ScrollView } from "react-native";
 import { useAppDispatch, useAppSelector } from "../../hook";
 import { useEffect, useState } from "react";
 import { fetchMyPosts } from "../../store/thunks/postsThunk";
 import PostReusable from "../../components/PostReusable";
+import { StyledContainer, StyledHomeBox } from "../../components/StyleContainer";
 
 function MyPosts() {
     const myPosts = useAppSelector(state => state.posts.myPosts.data || []);
@@ -21,8 +22,10 @@ function MyPosts() {
     );
 
     return (
-        <View className="flex-1 bg-kuBGColor p-4 pt-10 items-center">
-            <View className="w-full max-w-md bg-kuColor rounded-2xl p-6 shadow-lg">
+        
+        <StyledContainer>
+            
+            <StyledHomeBox>
                 <Text className="text-2xl font-semibold text-black text-center mb-4">My Posts</Text>
 
                 <TextInput
@@ -32,16 +35,17 @@ function MyPosts() {
                     className="bg-gray-100 rounded-full px-4 h-12 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300 w-full"
                     placeholderTextColor="#555"
                 />
-
-                {filteredMyPosts.length > 0 ? (
-                    filteredMyPosts.map((post, index) => (
-                        <PostReusable key={index} post={post} isMyPosts={true} />
-                    ))
-                ) : (
-                    <Text className="text-center text-gray-400 mt-4">No posts found</Text>
-                )}
-            </View>
-        </View>
+                <ScrollView className="w-full flex-grow" showsVerticalScrollIndicator={false}>
+                    {filteredMyPosts.length > 0 ? (
+                        filteredMyPosts.map((post, index) => (
+                            <PostReusable key={index} post={post} isMyPosts={true} />
+                        ))
+                    ) : (
+                        <Text className="text-center text-gray-400 mt-4">No posts found</Text>
+                    )}
+                </ScrollView>
+             </StyledHomeBox>
+        </StyledContainer>
     );
 }
 
