64db2a17d0aeb5a6b673756571ca1e29beffbc41
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-02-22 00:00:37 +0700

    modified: mb

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,12 +38,12 @@ function Home() {
                     placeholder="search title"
                     value={term}
                     onChangeText={(value) => setTerm(value)}
-                    className="bg-gray-100 rounded-full px-4 h-12 mt-10 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300 w-full"
+                    className="bg-gray-100 rounded-full px-4 h-12 mt-10 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300 w-full mb-4"
                     placeholderTextColor="#555"
                 />
                 <ScrollView className="w-full flex-grow" showsVerticalScrollIndicator={false}>
                     {filteredMyPosts.map((post, index) => (
-                        <PostReusable key={index} post={post} className="w-full"/>
+                        <PostReusable key={index} post={post}/>
                     ))}
                 </ScrollView>
             </StyledHomeBox>
