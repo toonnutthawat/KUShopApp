9819dc83e7f2fe99a111f03cb96a527a6c83554b
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-03-04 04:13:08 +0700

    modified: myfavProduct

--- src/pages/products/MyFavoriteProducts.tsx
+++ src/pages/products/MyFavoriteProducts.tsx
@@ -7,6 +7,7 @@ import Header from "../../components/Header";
 import ProductReusable from "../../components/ProductReusable";
 import { Product } from "../../API";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { hp, wp } from "../../helpers/common";
 
 function MyFavoriteProducts(){
     const dispatch = useAppDispatch()
@@ -31,23 +32,25 @@ function MyFavoriteProducts(){
         <StyledContainer>
             <View className="flex flex-row items-center">
                 <AntDesign name="heart" size={24} color="red" className="mr-4"/>
-                <Header title = "Favorite Products"showBackButton={false}></Header>    
+                <Header title = "บันทึกสินค้า"showBackButton={false}></Header>    
             </View>
             <StyledHomeBox>
                 <TextInput
-                    placeholder="Search title"
+                    placeholder="ค้นหาสินค้า..."
                     value={term}
                     onChangeText={(value) => setTerm(value)}
                     className="bg-gray-100 rounded-full px-4 h-12 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300 w-full"
                     placeholderTextColor="#555"
                 />
-                <ScrollView className="w-full flex-grow" showsVerticalScrollIndicator={false}>
+                <ScrollView contentContainerStyle={{flexDirection: 'row', flexWrap: 'wrap', marginLeft: 7, gap: hp(1),justifyContent: 'center'}} className="w-full flex-grow" showsVerticalScrollIndicator={false}>
                     {filteredMyFavoriteProducts.length > 0 ? (
                         filteredMyFavoriteProducts.map((product, index) => (
-                            <ProductReusable key={index} product={product} />
+                            <View style={{ width: hp(20), height: wp(60), marginBottom: hp(5)}}>
+                                <ProductReusable key={index} product={product} />
+                            </View>
                         ))
                     ) : (
-                        <Text className="text-center text-gray-400 mt-4">No posts found</Text>
+                        <Text className="text-center text-gray-400 mt-4">ไม่พบรายการ...</Text>
                     )}
                 </ScrollView>
             </StyledHomeBox>
