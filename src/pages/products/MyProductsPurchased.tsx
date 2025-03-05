00e3a1b84819fa96396306863e6f4b839cd259ea
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-03-05 16:37:15 +0700

    modified: Change Thai and styles

--- src/pages/products/MyProductsPurchased.tsx
+++ src/pages/products/MyProductsPurchased.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from "../../hook";
 import { fetchMyProductsPurchased } from "../../store/thunks/productsThunk";
 import Header from "../../components/Header";
 import ProductReusable from "../../components/ProductReusable";
+import { hp, wp } from "../../helpers/common";
 
 function MyProductsPurchased(){
     const dispatch = useAppDispatch()
@@ -29,23 +30,25 @@ function MyProductsPurchased(){
     return(
         <StyledContainer>
             <View className="flex flex-row items-center">
-                <Header title = "MyProductsPurchased"showBackButton={false}></Header>    
+                <Header title = "ประวัติการซื้อของฉัน"showBackButton={false}></Header>    
             </View>
             <StyledHomeBox>
                 <TextInput
-                    placeholder="Search title"
+                    placeholder="ค้นหาประวัติการซื้อของฉัน..."
                     value={term}
                     onChangeText={(value) => setTerm(value)}
                     className="bg-gray-100 rounded-full px-4 h-12 mb-4 border border-gray-300 focus:border-green-500 focus:ring focus:ring-green-300 w-full"
                     placeholderTextColor="#555"
                 />
-                <ScrollView className="w-full flex-grow" showsVerticalScrollIndicator={false}>
+                <ScrollView contentContainerStyle={{flexDirection: 'row', flexWrap: 'wrap', marginLeft: 7, gap: hp(1),justifyContent: 'center'}} className="w-full flex-grow" showsVerticalScrollIndicator={false}>
                     {filteredMyFavoriteProducts.length > 0 ? (
                         filteredMyFavoriteProducts.map((product, index) => (
-                            <ProductReusable key={index} product={product} />
+                            <View key={index} style={{ width: hp(34), height: wp(55), marginBottom: hp(5)}}>
+                                <ProductReusable key={index} product={product} />
+                            </View>
                         ))
                     ) : (
-                        <Text className="text-center text-gray-400 mt-4">No posts found</Text>
+                        <Text className="text-center text-gray-400 mt-4">ไม่พบรายการ...</Text>
                     )}
                 </ScrollView>
             </StyledHomeBox>
