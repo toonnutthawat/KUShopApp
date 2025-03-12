442e44c6139d56d201b5ecaaeb242686d520290e
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-03-12 17:41:59 +0700

    modified: ProductCategoryTab add pic

--- src/components/ProductCategoryTab.tsx
+++ src/components/ProductCategoryTab.tsx
@@ -5,22 +5,22 @@ function ProductCategoryTab({onSelectCategory}){
     return(
         <View className="flex flex-row">
             <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Electronics)}>
-                <Image source={require("../../assets/gadgets.png")} style={styles.categoryImg}></Image>
+                <Image source={require("../../assets/pic1-r.png")} style={styles.categoryImg}></Image>
             </Pressable>
             <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Clothing)}>
-                <Image source={require("../../assets/brand.png")} style={styles.categoryImg}></Image>
+                <Image source={require("../../assets/pic2-r.png")} style={styles.categoryImg}></Image>
             </Pressable>
             <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.HomeAppliances)}>
-                <Image source={require("../../assets/small-appliance.png")} style={styles.categoryImg}></Image>
+                <Image source={require("../../assets/pic3-r.png")} style={styles.categoryImg}></Image>
             </Pressable>
             <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Books)}>
-                <Image source={require("../../assets/book.png")} style={styles.categoryImg}></Image>
+                <Image source={require("../../assets/pic4-r.png")} style={styles.categoryImg}></Image>
             </Pressable>
             <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Toys)}>
-                <Image source={require("../../assets/toys.png")} style={styles.categoryImg}></Image>
+                <Image source={require("../../assets/pic5-r.png")} style={styles.categoryImg1}></Image>
             </Pressable>
             <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.ETC)}>
-                <Image source={require("../../assets/more.png")} style={styles.categoryImg}></Image>
+                <Image source={require("../../assets/pic6-r.png")} style={styles.categoryImg}></Image>
             </Pressable>
         </View>
     )
@@ -31,11 +31,18 @@ const styles = StyleSheet.create({
         height: 40,
         width: 40
     },
+    categoryImg1: {
+        width: 40,  // Set the width you want
+        height: 30, // Set the height you want
+        justifyContent:'center'
+      
+    },
     container: {
         backgroundColor: 'white',
         padding: 8,
         borderRadius: 10,
-        marginLeft: 5
+        marginLeft: 5,
+        justifyContent:'center'
     }
 })
 
