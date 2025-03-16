68c12e4a92997ba7d70ea0621f3b34db746f1b7b
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-03-16 16:12:25 +0700

    modified: add text styles and some img

--- src/components/ProductCategoryTab.tsx
+++ src/components/ProductCategoryTab.tsx
@@ -12,27 +12,27 @@ function ProductCategoryTab({onSelectCategory}){
             
                 <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Electronics)}>
                     <Image source={require("../../assets/pic1-r.png")} style={styles.categoryImg}></Image>
-                    <Text>อิเล็กทรอนิกส์</Text>
+                    <Text style={styles.categoryText}>อิเล็กทรอนิกส์</Text>
                 </Pressable>
                 <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Clothing)}>
                     <Image source={require("../../assets/pic2-r.png")} style={styles.categoryImg}></Image>
-                    <Text>เสื้อผ้า</Text>
+                    <Text style={styles.categoryText}>เสื้อผ้า</Text>
                 </Pressable>
                 <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.HomeAppliances)}>
                     <Image source={require("../../assets/pic3-r.png")} style={styles.categoryImg}></Image>
-                    <Text>เครื่องใช้ไฟฟ้า</Text>
+                    <Text style={styles.categoryText}>เครื่องใช้ไฟฟ้า</Text>
                 </Pressable>
                 <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Books)}>
                     <Image source={require("../../assets/pic4-r.png")} style={styles.categoryImg}></Image>
-                    <Text>หนังสือ</Text>
+                    <Text style={styles.categoryText}>หนังสือ</Text>
                 </Pressable>
                 <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.Toys)}>
                     <Image source={require("../../assets/pic5-r.png")} style={styles.categoryImg}></Image>
-                    <Text>ของเล่น</Text>
+                    <Text style={styles.categoryText}>ของเล่น</Text>
                 </Pressable>
                 <Pressable style={styles.container} onPress={() => onSelectCategory(ProductCategory.ETC)}>
-                    <Image source={require("../../assets/pic6-r.png")} style={styles.categoryImg}></Image>
-                    <Text>อื่นๆ</Text>
+                    <Image source={require("../../assets/pic6-r.png")} style={styles.categoryImg1}></Image>
+                    <Text style={styles.categoryText}>อื่นๆ</Text>
                 </Pressable>
             </ScrollView>
         </View>
@@ -43,6 +43,12 @@ const styles = StyleSheet.create({
     categoryImg: {
         height: 50,
         width: 50,
+        resizeMode:'contain',
+        justifyContent:'center'
+    },
+    categoryImg1: {
+        height: 50,
+        width: 35,
         resizeMode:'contain',
         justifyContent:'center'
     },
@@ -57,7 +63,14 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         marginLeft: 5,
         alignItems:'center'
-    }
+    },
+    categoryText: {
+        fontSize: 14, // Adjust size as needed
+        fontWeight: 'bold', // Make the text bold
+        color: '#333', // Dark gray for readability
+        marginTop: 5, // Spacing between image and text
+        textAlign: 'center', // Center the text
+    },
 })
 
 
