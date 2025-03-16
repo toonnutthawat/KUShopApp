491c299404d7c24e38cd06586a05b8d53e7c9569
BallJedsadakorn <jedsadakorn.boo@ku.th>
2025-03-16 16:08:54 +0700

    modified: change name

--- src/types/ProductCategory.ts
+++ src/types/ProductCategory.ts
@@ -2,7 +2,7 @@
 export enum ProductCategory {
     Electronics = "อิเล็กทรอนิกส์",
     Clothing = "เสื้อผ้า",
-    HomeAppliances = "เครื่องใช้ในบ้าน",
+    HomeAppliances = "เครื่องใช้ไฟฟ้า",
     Books = "หนังสือ",
     Toys = "ของเล่น",
     ETC = "อื่นๆ"
